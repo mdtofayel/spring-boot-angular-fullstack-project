2112d1e760c3a98226484c4b3875e06cf9a652bc
Md. Tofayel Ahmed <tofayel35-499@diu.edu.bd>
2021-06-10 16:04:41 +0200

    ecommerce frontend  starting commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,13 @@
+
+    export default {
+
+    oidc: {
+        clientId: '0oaudaargE7H9Rc9A5d6',
+        issuer: 'https://dev-02304906.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
+
+
